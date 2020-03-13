80501d0d63a4bc923f762285aaa91d1dd93a2f8a
Vassilios <vogiatzisvassilios@gmail.com>
2020-03-13 15:23:13 +0000

    [2.0.0] Fixing argument parsing.

--- src/medusa.cpp
+++ src/medusa.cpp
@@ -15,11 +15,20 @@ int main(int argc, char** argv) {
 	// Parsing the arguments provided by the user.
 	arguments.add_options()
 		("f, file", "File Name", cxxopts::value<std::string>())
-		("w, width", "Image Width", cxxopts::value<int>()->default_value("100"));
+		("w, width", "Image Width", cxxopts::value<int>()->default_value("100"))
+		("o, output", "Output File", cxxopts::value<std::string>()->default_value("medusa"));
 	auto result = arguments.parse(argc, argv);
 	
 	// Obtaining the values from the arguments.
-	std::string fileName = result["file"].as<std::string>();
+	std::string fileName = "";
+	if (result.count("file")) {
+		fileName = result["file"].as<std::string>();
+	} else {
+		printf("[Medusa] Mandatory argument file has not been provided.\n");
+		exit(1);
+	}
+
+	std::string outputName = result["output"].as<std::string>();
 	int imageWidth = result["width"].as<int>();
 
     // Checking if the file provided exists.
@@ -33,7 +42,7 @@ int main(int argc, char** argv) {
     resize(&image, imageWidth);
     grayify(&image);
     std::vector<std::string> pixels = getPixels(&image);
-    writeToFile(pixels, "image", imageWidth);
+    writeToFile(pixels, outputName, imageWidth);
 
     return 0;
 }
